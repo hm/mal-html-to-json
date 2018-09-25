ed001202676097b540c5e8742e39cd8bc047a1d6
todd <todd@unoapp.com>
2018-09-25 15:27:03 -0400

    get json data from anime with id
    get json data from anime with id

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http')
+const htmlToJson = require('html-to-json')
+
+http.createServer(async (req, res) => {
+  await htmlToJson.request(`https://myanimelist.net/anime${req.url}`, {
+    title: ($doc) => $doc.find('h1').text(),
+    banner: ($doc) => $doc.find(`img[itemprop="image"]`).attr('src'),
+    score: ($doc) => $doc.find(`span[itemprop="ratingValue"]`).text(),
+    scoredBy: ($doc) => $doc.find(`span[itemprop="ratingCount"]`).text(),
+    type: ($doc) => $doc.find(`span:contains('Type:')`).next().text(),
+    popularity: ($doc) => $doc.find(`span:contains('Popularity ')`).text().replace(/\D/g,''),
+    synopsis: ($doc) => $doc.find(`span[itemprop="description"]`).text(),
+    episodes: ($doc) => $doc.find(`span:contains('Episodes:')`).parent().text().replace(/\D/g,''),
+    status: ($doc) => $doc.find(`span:contains('Status:')`).parent().find('span').remove().end().text().replace(/\r?\n|\r/g, '').trim(),
+  }, function (err, result) {
+      const string = JSON.stringify(result)
+      res.write(string)
+  });
+
+  res.end()
+}).listen(1234)
+
+console.log('listening to port 1234')
