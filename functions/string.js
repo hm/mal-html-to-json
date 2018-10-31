5f87d7c73605fe2b1dd90223037d48f459210df2
x_x <x.x>
2018-10-31 11:18:58 -0400

    Fix bug with total returning undefined when last page is selected

--- functions/string.js
+++ functions/string.js
@@ -1,6 +1,6 @@
 const removeLineBreaks = (str) => str.replace(/(\r\n\t|\n|\r\t)/gm,"").trim()
 const ignoreNonNumeric = (str) => str.replace(/\D/g, '').trim()
-const getLastNumber    = (str) => str.split(' ')[str.split(' ').length - 1]
+const getLastNumber    = (str) => str.split(' ')[str.split(' ').length - 1].replace(/\D/g, '').trim()
 const getStringBetween = (str, firstChar, lastChar) => 
   str.substring(str.lastIndexOf(firstChar) + 1, str.lastIndexOf(lastChar))
 
